de1094e1d9ce02f55b21f98d9216237ed9922db0
Manuel Bugallo <marvinody@gmail.com>
2019-06-26 19:57:09 -0400

    feat: update on A*. still not working. huge memory usage and inefficient traversal?

--- client/sketches/astar.js
+++ client/sketches/astar.js
@@ -1,49 +1,42 @@
-import heap from './binaryHeap'
+import heap from 'heap'
 import UID from './id'
-class Node {
-  constructor(val) {
-    this.val = this
-    this.edgesTo = []
-    this.edgesFrom = []
-    this.f = Infinity
-    this.h = Infinity
+export class Node {
+  constructor(data) {
     this.id = UID.new()
-  }
-  addDirectionalEdgeTo(to, weight) {
-    const edge = new Edge(this, to, weight)
-    this.edgesTo.push(edge)
-    to.edgesFrom.push(edge)
+    this.data = {...data, id: this.id}
   }
 }
 
-class Edge {
-  constructor(from, to, weight) {
-    this.cost = weight
-    this.to = to
-    this.from = from
-  }
-}
-
-// graph should be an array of Nodes
+// graph should be a 2d array of nodes. neighbors are the 8 surrounding
 // connected by edges created by addDirectionalEdgeTo
-export default function search(graph, start, end, heuristic = () => 0) {
+export default function search(
+  graph,
+  start,
+  end,
+  getNeighborsFn = () => [],
+  costFn = () => 0,
+  heuristicFn = () => 0
+) {
   // set of nodes already evaluated
   const closed = new Set()
-  // currently discovered nodes that are not evaluated yet
-  // takes a scoring function
-  const open = new heap(n => n.f)
-
   // will represent paths to the "best" node
   const cameFrom = new Map()
   // holds the raw edge weight, smaller means more optimal
-  const gScore = new Map(graph.map(n => [n.id, Infinity]))
+  const gScore = new Map(graph.flat().map(n => [n.id, Infinity]))
   // holds the combined g+h weight, smaller means more optimal
-  const fScore = new Map(graph.map(n => [n.id, Infinity]))
+  const fScore = new Map(graph.flat().map(n => [n.id, Infinity]))
+
+  const idToNode = new Map(graph.flat().map(n => [n.id, n]))
+
+  // currently discovered nodes that are not evaluated yet
+  // takes a scoring function
+  const open = new heap((n1, n2) => fScore.get(n1.id) - fScore.get(n2.id))
+  open.push(start)
 
   // the cost to go to itself is 0
   gScore.set(start.id, 0)
   // combined score would be 0 + h(x)
-  fScore.set(start.id, heuristic(start))
+  fScore.set(start.id, heuristicFn(start.data))
 
   open.push(start)
 
@@ -52,8 +45,48 @@ export default function search(graph, start, end, heuristic = () => 0) {
     const currNode = open.pop()
 
     // End case -- result has been found, return the traced path.
-    if (currentNode === end) {
-      return pathTo(currentNode)
+    if (currNode === end) {
+      return pathTo(end, start, cameFrom, idToNode)
     }
+
+    closed.add(currNode.id)
+    getNeighborsFn(currNode).forEach(neighbor => {
+      const cost = costFn(currNode.data, neighbor.data)
+      const nextNode = neighbor
+      if (closed.has(nextNode.id)) {
+        return
+      }
+
+      const newGScore = gScore.get(currNode.id) + cost
+      if (!open.has(nextNode.id)) {
+        open.push(nextNode) // new neighbor, visit eventually
+      } else if (newGScore >= gScore.get(nextNode.id)) {
+        return // return cause worse score and don't wanna update it
+      }
+
+      cameFrom.set(nextNode.id, currNode.id)
+      gScore.set(nextNode.id, newGScore)
+      const newHScore = heuristicFn(
+        nextNode.data,
+        currNode.data,
+        start.data,
+        end.data
+      )
+      fScore.set(nextNode.id, newGScore + newHScore)
+    })
   }
+  console.log('end, start', end, start)
+  return pathTo(end, start, cameFrom, idToNode)
+}
+
+function pathTo(endID, startID, cameFrom, idToNode) {
+  let path = []
+  console.log(arguments)
+  let tail = idToNode.get(startID)
+  while (tail.id !== startID || tail.id !== endID) {
+    path = [...path, tail.data]
+    const nodeID = cameFrom.get(tail.id)
+    tail = idToNode.get(nodeID)
+  }
+  return path
 }
