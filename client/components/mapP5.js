b05894f0ed3ac12ed3dbedf20956db12ddc4f142
sam-peach <sampeachart@gmail.com>
2019-07-13 15:54:00 -0400

    style: created loading animation

--- client/components/mapP5.js
+++ client/components/mapP5.js
@@ -5,11 +5,10 @@ import marsMapSketch from '../sketches/marsMapSketch'
 const MapP5 = props => {
   return (
     <div>
-      <P5Wrapper sketch={marsMapSketch} className="myCanvas">
-        <div id="p5_loading" className="loadingclass">
-          <img src="https://media.giphy.com/media/55IcZKfutOQZG/giphy.gif" />
-        </div>
-      </P5Wrapper>
+      <P5Wrapper sketch={marsMapSketch} className="myCanvas" />
+      <div id="p5_loading" className="loadingclass">
+        <img src="https://www.dokkan.ly/Themes/Dokkan/Content/images/dokkanloader.gif" />
+      </div>
     </div>
   )
 }
